759d9f219661c4eecc7d3f169ba4a5e425eba9ef
PIO-VIA <piodjiele@gmail.com>
2025-09-01 21:38:24 +0100

    init projet

---
+++ src/test/java/com/yooyob/erp/FacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yooyob.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
